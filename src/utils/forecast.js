c5433f1547b9c56ffd63f01debc1a0e99e55a4dc
Yannick Wiley <yannick.wiley@gmail.com>
2019-08-20 00:27:17 -0400

    forecast messege changed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,23 @@
 const request = require('request')
+const timeConversion = (millisec) => {
+    let seconds = (millisec / 1000).toFixed(1);
+
+    let minutes = (millisec / (1000 * 60)).toFixed(1);
+
+    let hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+
+    let days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
+
+    if (seconds < 60) {
+        return seconds + " Sec";
+    } else if (minutes < 60) {
+        return minutes + " Min";
+    } else if (hours < 24) {
+        return hours + " Hrs";
+    } else {
+        return days + " Days"
+    }
+}
 
 const forecast = (long, lati, callback) => {
     const url = "https://api.darksky.net/forecast/fec769c1f4efe1c70400649869793b92/" + long + "," + lati + "?"
@@ -10,7 +29,7 @@ const forecast = (long, lati, callback) => {
         } else if (body.error, undefined) {
             callback('Unable to find location. Try another search', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + 'It is currently ' + currently.temperature + ' degress out. ' + 'There is ' + currently.precipProbability + '% chance of rain')
+            callback(undefined, body.daily.data[0].summary + 'It is currently ' + currently.temperature + ' degress out. ' + 'There is ' + currently.precipProbability + '% chance of rain. ' + 'Tempature High will be ' + body.daily.data[0].temperatureHigh + ' degrees' + ' and tempature low will be ' + body.daily.data[0].temperatureLow + ' degrees')
         }
 
         
